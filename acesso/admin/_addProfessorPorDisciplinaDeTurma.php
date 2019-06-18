d7b9451c7830a3057d651ab67456f042e39dbdab
Paul Richard <paulrpm@gmail.com>
2019-06-18 10:50:35 -0300

    adicionar nota inicial 0 para cada aluno da turma da materia adicionada por professor

--- acesso/admin/_addProfessorPorDisciplinaDeTurma.php
+++ acesso/admin/_addProfessorPorDisciplinaDeTurma.php
@@ -24,6 +24,27 @@ if (isset($_SESSION['tipo'])){
 			'idTurma' => $turma,
 		]);
 		
+                $alunosQuery = $db->query("SELECT * FROM aluno where idTurma = $turma");
+                $alunos = $alunosQuery->fetchAll(PDO::FETCH_ASSOC);
+                
+                foreach ($alunos as $aluno) {
+                    $nota = $db->prepare("INSERT INTO notaporaluno (idAluno, idDisciplina, idTurma, nota1, nota2, nota3, nota4, rec1, rec2, rec3, rec4) VALUES (:idAluno, :idDisciplina, :idTurma, :nota1, :nota2, :nota3, :nota4, :rec1, :rec2, :rec3, :rec4)");
+		
+                    $nota->execute([
+                            'idAluno' => $aluno['idAluno'],
+                            'idDisciplina' => $disciplina,
+                            'idTurma' => $turma,
+                            'nota1' => 0,
+                            'nota2' => 0,
+                            'nota3' => 0,
+                            'nota4' => 0,
+                            'rec1' => 0,
+                            'rec2' => 0,
+                            'rec3' => 0,
+                            'rec4' => 0,
+                    ]);
+                }
+                
 		header("Location: cadProfessor.php");
 
 	}
