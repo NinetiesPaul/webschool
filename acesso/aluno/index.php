b1687f0f2075f8f70ae1078cbc9003b2a98e6ca2
Paul Richard <paulrpm@gmail.com>
2019-09-25 22:49:41 -0300

    adicionado links seo no portal do aluno

--- acesso/aluno/index.php
+++ acesso/aluno/index.php
@@ -49,7 +49,7 @@ if (isset($_SESSION['tipo'])) {
 
         <div class="container">
             <div class="jumbotron text-center">
-                <a href="visualizarTurmas.php" class="btn btn-light btn btn-block">Turmas</a>
+                <a href="turmas" class="btn btn-light btn btn-block">Turmas</a>
                 <a href="../perfil.php" class="mobile btn btn-dark btn btn-block" >Meu Perfil</a>
                 <a href="../../logout.php" class="mobile btn btn-danger btn btn-block" >Sair</a>
                 <!-- <a href="solicitacoes.php">Fazer solicitações ao administrador</a><br/> -->
