a3b279fad8f6d29c3cad39cb89906a0efe25095f
Paul Richard <paulrpm@gmail.com>
2019-09-28 16:03:34 -0300

    modificado tela de visualização de notas; adicionado botão para visualizar faltas

--- acesso/aluno/visualizarTurmas.php
+++ acesso/aluno/visualizarTurmas.php
@@ -23,6 +23,30 @@ if (isset($_SESSION['tipo'])) {
         <link href="../../res/navbar.css" rel="stylesheet">
         <script src="../../res/jquery.js">
         </script>
+        <script>
+        function pesquisarFaltas(aluno,turma,disciplina) {
+            $.ajax({
+                type: "POST",
+                url: "../../data/pesquisarFaltas.php",
+                data:'aluno='+aluno+'&turma='+turma+'&disciplina='+disciplina,
+                success: function(data ){
+                    $("#result").html(data);
+                }
+            });
+        }
+        
+        $(document).ready(function(){
+            $(".faltas").click(function(){
+                dado = this.id;
+                dado = dado.split('.');
+                aluno = dado[0];
+                turma = dado[1];
+                disciplina = dado[2];
+                pesquisarFaltas(aluno,turma,disciplina);
+                
+            });
+        });
+        </script>
         <title>Aluno :: Minhas Turmas</title>
     </head>
     <body>
@@ -64,7 +88,7 @@ if (isset($_SESSION['tipo'])) {
                     echo '<table style="margin-left: auto; margin-right: auto; font-size: 13;" class="table">';
 
                     foreach ($notas as $nota) {
-                        echo '<tr><td>'.pegarDisciplina($nota->idDisciplina).'</td>';
+                        echo '<tr><td><strong>'.pegarDisciplina($nota->idDisciplina).'<strong></td>';
                         echo '<td>Nota 1: '.$nota->nota1.'</td>';
                         echo '<td>Nota 2: '.$nota->nota2.'</td>';
                         echo '<td>Nota 3: '.$nota->nota3.'</td>';
@@ -73,6 +97,11 @@ if (isset($_SESSION['tipo'])) {
                         echo '<td>Rec 2: '.$nota->rec2.'</td>';
                         echo '<td>Rec 3: '.$nota->rec3.'</td>';
                         echo '<td>Rec 4: '.$nota->rec4.'</td>';
+                        echo "<td>
+                            <button class='btn btn-sm btn-info faltas' data-toggle='modal' data-target='#modalExemplo' id='$alunoQuery->idAluno.$turma.$nota->idDisciplina'>
+                                Faltas
+                            </button>
+                        </td>";
                         //echo "<td>Faltas: <a href='verFaltas.php?a[]=$alunoQuery->idAluno&a[]=$nota->idDisciplina&a[]=$nota->idTurma'>".pegarFaltasDoAluno($userId, $nota->idDisciplina, $nota->idTurma)."</a></td>";
                         echo '</tr>';
                     }
@@ -84,7 +113,29 @@ if (isset($_SESSION['tipo'])) {
             </div>
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <!-- Modal -->
+        <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel"><strong>Faltas</strong></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div id="result">
+                    Resultado aqui.
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>	
     </body>
