da2c11a468436295530ca5cd8feeb0f45eb8b75c
Paul Richard <paulrpm@gmail.com>
2019-09-28 20:44:54 -0300

    seo e correção de redirect nas paginas do portal do responsavel

--- acesso/responsavel/visualizarAluno.php
+++ acesso/responsavel/visualizarAluno.php
@@ -4,7 +4,7 @@ session_start();
 if (isset($_SESSION['tipo'])) {
     $tipo = $_SESSION['tipo'];
     if ($tipo != "responsavel") {
-        header('Location: index.php');
+        header('Location: ../index.php');
     } else {
         $userId = $_SESSION['user_id'];
         
@@ -69,7 +69,7 @@ if (isset($_SESSION['tipo'])) {
                             <!-- <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Something else here</a> -->
-                            <a class="dropdown-item" href="../../logout.php">Sair</a>
+                            <a class="dropdown-item" href="../../../logout">Sair</a>
                             <!-- <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#">Something else here</a> -->
