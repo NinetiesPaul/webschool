20c0152709d3e023d1b7b54b9b7eb8b72da501d0
Paul Richard <paulrpm@gmail.com>
2019-09-28 18:38:55 -0300

    listando faltas do aluno por matéria

--- acesso/responsavel/visualizarAluno.php
+++ acesso/responsavel/visualizarAluno.php
@@ -22,17 +22,41 @@ if (isset($_SESSION['tipo'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta charset="UTF8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link href="../../res/css.css" rel="stylesheet">
-        <link href="../../res/navbar.css" rel="stylesheet">
-        <script src="../../res/jquery.js">
+        <link href="../../../res/css.css" rel="stylesheet">
+        <link href="../../../res/navbar.css" rel="stylesheet">
+        <script src="../../../res/jquery.js">
         </script>
-        <script src="../../res/detect.js">
+        <script src="../../../res/detect.js">
+        </script>
+        <script>
+        function pesquisarFaltas(aluno,turma,disciplina) {
+            $.ajax({
+                type: "POST",
+                url: "../../../data/pesquisarFaltas.php",
+                data:'aluno='+aluno+'&turma='+turma+'&disciplina='+disciplina,
+                success: function(data ){
+                    $("#result").html(data);
+                }
+            });
+        }
+        
+        $(document).ready(function(){
+            $(".faltas").click(function(){
+                dado = this.id;
+                dado = dado.split('.');
+                aluno = dado[0];
+                turma = dado[1];
+                disciplina = dado[2];
+                pesquisarFaltas(aluno,turma,disciplina);
+                
+            });
+        });
         </script>
         <title>Responsável :: Detalhes do Aluno</title>
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="index.php">webSchool</a>
+            <a class="navbar-brand" href="../home">webSchool</a>
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
@@ -40,7 +64,7 @@ if (isset($_SESSION['tipo'])) {
                             Logado como <?php echo pegarNomeDoResponsavel($userId); ?>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="index.php">Home</a>
+                            <a class="dropdown-item" href="../home">Home</a>
                             <a class="dropdown-item" href="../perfil.php">Perfil</a>
                             <!-- <a class="dropdown-item" href="#">Another action</a>
                             <div class="dropdown-divider"></div>
@@ -74,12 +98,12 @@ if (isset($_SESSION['tipo'])) {
                 foreach ($turmas as $turma) {
                     $turma = $turma->idTurma;
                     echo pegarTurma($turma).'<br/>';
-                    $notasQuery = $db->query("select * from notaporaluno where idAluno=$aluno and idTurma=$turma");
+                    $notasQuery = $db->query("select * from notaporaluno where idAluno=$aluno and idTurma=$turma order by idDisciplina");
                     $notas = $notasQuery->fetchAll(PDO::FETCH_OBJ);
                     
                     echo '<table style="margin-left: auto; margin-right: auto; font-size: 13;" class="table">';
                     foreach ($notas as $nota) {
-                        echo '<tr><td>'.pegarDisciplina($nota->idDisciplina).'</td>';
+                        echo '<tr><td><strong>'.pegarDisciplina($nota->idDisciplina).'<strong></td>';
                         echo '<td>Nota 1: '.$nota->nota1.'</td>';
                         echo '<td>Nota 2: '.$nota->nota2.'</td>';
                         echo '<td>Nota 3: '.$nota->nota3.'</td>';
@@ -88,7 +112,11 @@ if (isset($_SESSION['tipo'])) {
                         echo '<td>Rec 2: '.$nota->rec2.'</td>';
                         echo '<td>Rec 3: '.$nota->rec3.'</td>';
                         echo '<td>Rec 4: '.$nota->rec4.'</td>';
-                        //echo "<td>Faltas: <a href='faltasDoAluno.php?a[]=$aluno&a[]=$nota->idDisciplina&a[]=$nota->idTurma'>".pegarFaltasDoAluno($alunoQuery->idUsuario, $nota->idDisciplina, $nota->idTurma)."</a></td>";
+                        echo "<td>
+                            <button class='btn btn-sm btn-info faltas' data-toggle='modal' data-target='#modalExemplo' id='$aluno.$turma.$nota->idDisciplina'>
+                                Faltas
+                            </button>
+                        </td>";
                         echo '</tr>';
                     }
                     echo '</table>';
@@ -99,7 +127,29 @@ if (isset($_SESSION['tipo'])) {
             </div>
         </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <!-- Modal -->
+        <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel"><strong>Faltas</strong></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div id="result">
+                    Resultado aqui.
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     </body>
