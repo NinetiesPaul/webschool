f7c0fc770e19211c23b43bdace857bb458a990fe
Paul Richard <paulrpm@gmail.com>
2019-10-05 20:49:06 -0300

    refatoração da exibição de alunos

--- data/pesquisarFrequencia.php
+++ data/pesquisarFrequencia.php
@@ -15,7 +15,7 @@ if (! empty($_POST)) {
     $results = [];
     for ($i = 1; $i < $days+1; $i++) {
         $date = $ano.'-'.$mes.'-'.$i;
-        $dates[] = $date;
+        $dates[] = new DateTime($date);
     }
     
     echo '<p/>';
@@ -34,45 +34,43 @@ if (! empty($_POST)) {
     echo "<table class='table table-striped table-borderless table-responsive'>";
     echo "<thead class='thead-dark'>";
     echo "<tr>";
-    echo "<th scope='col'> Data </th>";
-    foreach ($alunos as $aluno) {
-        echo "<th scope='col'>$aluno->nome</th>";
+    echo "<th scope='col'>  </th>";
+    foreach ($dates as $date) {
+        echo "<th scope='col'>".$date->format('d')."</th>";
     }
     echo "</tr>";
     echo "</thead>";
     echo "<tbody>";
-    foreach ($dates as $date) {
+    foreach ($alunos as $aluno) {
         echo "<tr>";
-        echo "<th scope='row'>".$date."</th>";
-        foreach ($alunos as $aluno) {
+        echo "<td>$aluno->nome</td>";
+        foreach ($dates as $date) {
             $diarioQuery = $db->query("
-            select *
-            from diariodeclasse
-            where idAluno = $aluno->idAluno
-            and diariodeclasse.idDisciplina = $disc and diariodeclasse.idTurma = $turma
-            and dataDaFalta = '$date'
+                select *
+                from diariodeclasse
+                where idAluno = $aluno->idAluno
+                and diariodeclasse.idDisciplina = $disc and diariodeclasse.idTurma = $turma
+                and dataDaFalta = '".$date->format('Y-m-d')."'
             ");
             $diario = $diarioQuery->fetch(PDO::FETCH_OBJ);
-            
-            $presenca = 'Falta';
-            
-            $date = explode('-', $date);
-            
+
+            $presenca = "<span class='glyphicon glyphicon-remove'></span>";
+
+            $date = explode('-', $date->format('Y-m-d'));
+
             if ($context === 'professor') {
                 $link = "../../criar-presenca/$date[0]/$date[1]/$date[2]/$aluno->idAluno/$aluno->idDisciplina/$aluno->idTurma";
             }
-            
-            if ($diario) {
-                if ($diario->presenca == 1) {
-                    $presenca = 'Presente';
-                    if ($context === 'professor') {
-                        $link = "../../presenca/$diario->idDiario";
-                    }
+
+            if ($diario && $diario->presenca == 1) {
+                $presenca = "<span class='glyphicon glyphicon-ok'></span>";
+                if ($context === 'professor') {
+                    $link = "../../presenca/$diario->idDiario";
                 }
             }
-            echo "<th scope='row'>";
+            echo "<td>";
             echo ($context === 'professor') ? "<a href='$link' id='presenca'>$presenca</a>" : $presenca;
-            echo "</th>";
+            echo "</td>";
         }
     }
     echo "</tr>";
