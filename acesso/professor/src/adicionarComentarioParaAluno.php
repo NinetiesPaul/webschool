16a302a4dc4e1389c6387521a1effe6874c64b22
Paul Richard <paulrpm@gmail.com>
2019-10-19 13:02:01 -0300

    adicionado lógica para upload de arquivo anexado

--- acesso/professor/src/adicionarComentarioParaAluno.php
+++ acesso/professor/src/adicionarComentarioParaAluno.php
@@ -12,41 +12,80 @@ include '../../../includes/php/functions.php';
 include '../../../includes/php/conn.php';
 
 if (!empty($_POST)) {
+    
+    
+    $dataComentario = $_POST['data'];
+    
+    $arquivos = false;
+    $file_name = '';
+    
+    if (empty($_FILES['fileToUpload']['error'])) {
+        echo 'salvar arquivo';
+        
+        $arquivos = true;
+        $file = $_FILES["fileToUpload"]["name"];
+        $file = str_replace(" ", "_", $file);
+        $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+        
+        if (in_array($ext, ['pdf', 'txt', 'mp4', 'mp3', 'ogg'])) {
+            $path='../../../uploads/files/';
+            
+            if (!file_exists($path)) {
+                mkdir($path);
+            }
+            
+            $data = getdate();
+            $long = $data[0];
+            $file_name=$long.'-'.$file;
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path . $file_name);
+            $urlFinal = '../../../uploads/files/'.$file_name;
+            $urlThumbFinal = '';
+        }
+        
+        if (in_array($ext, ['jpg', 'jpeg'])) {
+            $path='../../../uploads/images/';
+            $pathThumb='../../../uploads/images/thumbs/';
+
+            if (!file_exists($path)) {
+                mkdir($path);
+            }
+            if (!file_exists($pathThumb)) {
+                mkdir($pathThumb);
+            }
+
+            $data = getdate();
+            $long = $data[0];
+            $file_name=$long.'-'.$file;
+            $file_name_thumb=$long.'-thumbs-'.$file;
+
+            $images = $_FILES["fileToUpload"]["tmp_name"];
+            $new_images = $long.'-thumbs-'.$file;
+            $width=200;
+            $size=GetimageSize($images);
+            $height=round($width*$size[1]/$size[0]);
+            $images_orig = ImageCreateFromJPEG($images);
+            $photoX = ImagesX($images_orig);
+            $photoY = ImagesY($images_orig);
+            $images_fin = ImageCreateTrueColor($width, $height);
+            ImageCopyResampled($images_fin, $images_orig, 0, 0, 0, 0, $width+1, $height+1, $photoX, $photoY);
+            ImageJPEG($images_fin, $pathThumb.$new_images);
+            ImageDestroy($images_orig);
+            ImageDestroy($images_fin);
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path . $file_name);
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $path . $file_name_thumb);
+
+            $urlFinal = '../uploads/images/'.$file_name;
+            $urlThumbFinal = '../uploads/images/thumbs/'.$file_name_thumb;    
+        }
+    }
+    
     $aluno = $_POST['aluno'];
     $turma = $_POST['turma'];
     $disciplina = $_POST['disciplina'];
-    $data = $_POST['data'];
     $prof = $_POST['prof'];
     $mensagem = $_POST['comentario'];
     
-    $return = explode('-', $data);
-
-    $comentarioQuery = $db->query("
-        select *
-        from diario_de_classe
-        where aluno = $aluno
-        and disciplina = $disciplina
-        and turma = $turma
-        and data = '$data'
-        and professor = $prof
-        and contexto = 'observacao'
-    ");
-    $comentario = $comentarioQuery->fetch(PDO::FETCH_OBJ);    
-    
-    if ($comentario) {
-        $user = $db->prepare("
-            UPDATE diario_de_classe
-            SET observacao = :mensagem
-            where id=$comentario->id
-        ");
-
-        $res = $user->execute([
-            'mensagem' => $mensagem
-        ]);
-        
-        header("Location: ../comentario/$return[0]/$return[1]/$return[2]/$aluno/$disciplina/$turma");
-        exit();
-    }
+    $return = explode('-', $dataComentario);
     
     $user = $db->prepare("
         INSERT INTO diario_de_classe
@@ -59,9 +98,25 @@ if (!empty($_POST)) {
         'aluno' => $aluno,
         'disciplina' => $disciplina,
         'mensagem' => $mensagem,
-        'data' => $data,
+        'data' => $dataComentario,
         'professor' => $prof,
     ]);
+    
+    if ($arquivos) {
 
+        $id = (int) $db->lastInsertId();
+        
+        $fileQuery = $db->prepare("
+            INSERT INTO arquivos (nome, endereco_thumb, endereco, contexto, diario, descricao, data) VALUES (:nome, :endereco_thumb, :endereco, 'ddc', $id, '', :data)
+        ");
+
+        $fileQuery->execute([
+            'nome' => $file_name,
+            'endereco_thumb' => $urlThumbFinal,
+            'endereco' => $urlFinal,
+            'data' => $dataComentario,
+        ]);
+    } 
+    
     header("Location: ../comentario/$return[0]/$return[1]/$return[2]/$aluno/$disciplina/$turma");
 }
