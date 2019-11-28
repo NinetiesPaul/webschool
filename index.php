1773e6ddccb8b5e55180258c7f228aac39dfd58a
Paul Richard <paulrpm@gmail.com>
2019-11-28 13:48:03 -0300

    secundary classes

--- index.php
+++ index.php
@@ -1,68 +1,20 @@
 <?php
 
-session_start();
+use Pecee\SimpleRouter\SimpleRouter;
 
-if (isset($_SESSION['tipo'])) {
-    $tipo = $_SESSION['tipo'];
-    if ($tipo == "admin") {
-        header('Location: acesso/admin/home');
-    }
-    if ($tipo == "responsavel") {
-        header('Location: acesso/responsavel/home');
-    }
-    if ($tipo == "professor") {
-        header('Location: acesso/professor/home');
-    }
-    if ($tipo == "aluno") {
-        header('Location: acesso/aluno/home');
-    }
-} else {
-    ?>
+include 'vendor/autoload.php';
 
-<html lang="en">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <meta charset="UTF8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <link href="includes/css/navbar.css" rel="stylesheet">
-        <link href="includes/css/login.css" rel="stylesheet">
-        <script src="includes/js/jquery.js"></script>
-        <title>webSchool :: Home Page</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="index.php">webSchool</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="PokeXchange">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </nav>
+/* Load external routes file */
+require_once 'helpers.php';
+require_once 'routes.php';
 
-        <div class="container text-center">
-            <h3>Bem-vindo</h3>
+/**
+ * The default namespace for route-callbacks, so we don't have to specify it each time.
+ * Can be overwritten by using the namespace config option on your routes.
+ */
 
-            <div class="login">
-                <form action="login" method="post" class="form-signin">
-                    <input type="text" name="email" placeholder="Digite seu login" class="form-control" >
-                    <input type="password" name="password" placeholder="Digite sua senha" class="form-control" >
-                        <select name="tipo" class="form-control" required>
-                            <option value=''>Selecione tipo de login</option>
-                            <option value='admin'>Admin</option>
-                            <option value='professor'>Professor</option>
-                            <option value='responsavel'>Responsavel</option>
-                            <option value='aluno'>Aluno</option>
-                        </select><br/>
-                    <input type="submit" value="Login" class="btn btn-lg btn-primary btn-block" ><br/>
-                </form>
-            </div>
-        </div>
 
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>			
-    </body>
-</html>
+SimpleRouter::setDefaultNamespace('Controllers');
 
-<?php
-}
-
-?>
+// Start the routing
+SimpleRouter::start();
