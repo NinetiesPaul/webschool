64823a7b4fd72a7912fba7b83a062dcab6ef2593
Paul Richard <paulrpm@gmail.com>
2019-11-29 15:44:54 -0300

    Update DB.php

--- app/DB/DB.php
+++ app/DB/DB.php
@@ -1,19 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of DB
- *
- * @author Paul Richard
- */
-
-declare(strict_types=1);
-
 namespace App\DB;
 
 use PDO;
@@ -28,7 +14,7 @@ class DB extends PDO {
         $dbname_db = 'webschool';
         $user_db = 'root';
         $password_db = '';
-        parent::__construct($dsn, $username, $password, $options);
+        parent::__construct("mysql:host=$localhost_db; dbname=$dbname_db", $user_db, $password_db, []);
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 }
