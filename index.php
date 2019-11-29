907e9b65e759e25868296e2fa89081618f87bf70
Paul Richard <paulrpm@gmail.com>
2019-11-29 15:46:02 -0300

    modified client php

--- index.php
+++ index.php
@@ -4,17 +4,9 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 include 'vendor/autoload.php';
 
-/* Load external routes file */
 require_once 'helpers.php';
 require_once 'routes.php';
 
-/**
- * The default namespace for route-callbacks, so we don't have to specify it each time.
- * Can be overwritten by using the namespace config option on your routes.
- */
-
-
 SimpleRouter::setDefaultNamespace('Controllers');
 
-// Start the routing
 SimpleRouter::start();
