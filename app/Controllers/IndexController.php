97fcfc558765be0ea376e76fce01fa6c65890b4a
Paul Richard <paulrpm@gmail.com>
2019-11-30 23:55:11 -0300

    removido codigo de teste

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -27,7 +27,6 @@ class IndexController {
     public function index()
     {
         session_start();
-        
         if (isset($_SESSION['tipo'])) {
             $tipo = $_SESSION['tipo'];
             if ($tipo == "admin") {
@@ -45,81 +44,6 @@ class IndexController {
         }
         
         $templateFinal 	= $this->template->getTemplate('index.html');
-        
         echo $templateFinal;
-    }
-    
-    public function list()
-    {
-        $arrTags = array(
-            'titulo'=>'Dicas de PHP',
-            'artigo_titulo'=>'Templates em PHP - Dicas de PHP',
-            'artigo_conteudo'=>'Demonstração de como criar um sistema simples de templates com PHP',
-            'rodape'=>'© Dicas de PHP - Todos direitos reservados 2012 ',
-            'item_list'=> [
-                1 => 'item 1',
-                2 => 'item 2'
-            ]
-        );
-        
-        $template = self::getTemplate('listing.html');
-        $templateFinal 	= self::parseTemplate( $template, $arrTags );
-        
-        echo $templateFinal;
-    }
-    
-    public function form()
-    {
-        $arrTags = array(
-            'titulo'=>'Dicas de PHP',
-            'artigo_titulo'=>'Templates em PHP - Dicas de PHP',
-            'artigo_conteudo'=>'Demonstração de como criar um sistema simples de templates com PHP',
-            'rodape'=>'© Dicas de PHP - Todos direitos reservados 2012 '
-        );
-        
-        $template = self::getTemplate('form.html');
-        $templateFinal 	= self::parseTemplate( $template, $arrTags );
-        
-        echo $templateFinal;
-    }
-    
-    public function fromForm()
-    {
-        $arrTags = array(
-            'titulo'=>'Dicas de PHP',
-            'artigo_titulo'=>'Templates em PHP - Dicas de PHP',
-            'artigo_conteudo'=>'Demonstração de como criar um sistema simples de templates com PHP',
-            'rodape'=>'© Dicas de PHP - Todos direitos reservados 2012 ',
-            'post_data' => json_encode($_POST)
-        );
-        
-        $template = self::getTemplate('fromform.html');
-        $templateFinal 	= self::parseTemplate( $template, $arrTags );
-        
-        echo $templateFinal;
-    }
-    
-    private function getTemplate( $template, $folder = "web/" ) 
-    {
-        $arqTemp = $folder.$template; // criando var com caminho do arquivo
-        $content = '';
-
-        if ( is_file( $arqTemp ) ) // verificando se o arq existe
-            $content = file_get_contents( $arqTemp ); // retornando conteúdo do arquivo
-
-        return $content;
-    }
-    
-    private function parseTemplate( $template, $array ) 
-    {
-        foreach ($array as $a => $b) {// recebemos um array com as tags 
-            if (strpos ($a, 'list')) {
-                $template = str_replace( '{'.$a.'}', json_encode($b), $template );
-            } else {
-                $template = str_replace( '{'.$a.'}', $b, $template );
-            }
-        }
-
-        return $template; // retorno o html com conteúdo final
     }
 }
