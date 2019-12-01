c4cf78dae2971a9e865e1a6cf7dadbcb711a03e3
Paul Richard <paulrpm@gmail.com>
2019-11-30 23:55:30 -0300

    adicionado metodos referentes ao portal de responsaveis do admin

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -5,16 +5,22 @@ namespace App\Controllers;
 use App\Templates;
 use App\DB\DB;
 use PDO;
+use App\Util;
 
 class AdminController {
     
     protected $template;
     
     protected $connection;
+    
+    protected $util;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->template = new Templates();
         $this->connection = new DB;
+        $this->util = new Util();
+        $this->util->userPermission('admin');
     }
     
     public function index()
@@ -35,14 +41,207 @@ class AdminController {
         echo $templateFinal;
     }
     
-    public function responsaveis()
+    public function verResponsaveis()
     {
-        $templateFinal 	= $this->template->getTemplate('admin/responsaveis.html');
+        $responsavelQuery = $this->connection->query("select usuario.* from usuario, responsavel where usuario.id=responsavel.usuario");
+        $responsavelQuery = $responsavelQuery->fetchAll(PDO::FETCH_OBJ);
+        
+        $responsaveis = ''; 
+        
+        foreach ($responsavelQuery as $responsavel) {
+            $is_deleted = ($responsavel->is_deleted) ? "<span class='glyphicon glyphicon-remove'></span> " : "<span class='glyphicon glyphicon-ok'></span> ";
+            $responsaveis .= 
+             "<tr id='row-$responsavel->id'><td>$responsavel->nome </td>
+             <td>$is_deleted<a href='responsavel/$responsavel->id' class='btn btn-info btn-sm btn-sm'><span class='glyphicon glyphicon-edit'></span> Editar</a> </td>
+             <td><button class='btn btn-danger btn-sm' id='deletar' value='$responsavel->id'><span class='glyphicon glyphicon-remove'></span> Deletar</button></td></tr>";
+        }
+        
+        $args = [
+          'RESPONSAVEIS' => $responsaveis  
+        ];
+        
+        $template 	= $this->template->getTemplate('admin/responsaveis.html');
+        $templateFinal = $this->template->parseTemplate($template, $args);
         echo $templateFinal;
     }
     
-    public function verTurmas()
+    public function verResponsavel(int $idResponsavel)
     {
+        $responsavelQuery = $this->connection->query("select usuario.*,responsavel.id as responsavel from usuario, responsavel where usuario.id=responsavel.usuario and responsavel.usuario = $idResponsavel");
+        $responsavel = $responsavelQuery->fetch(PDO::FETCH_OBJ);
+
+        $alunosQuery = $this->connection->query("select usuario.* from usuario, aluno where usuario.id = aluno.usuario");
+        $alunosQuery = $alunosQuery->fetchAll(PDO::FETCH_OBJ);
+
+        $alunos = '';
+        foreach ($alunosQuery as $aluno) {
+            $alunos .= "<option value='$aluno->id'>$aluno->nome (".$this->util->pegarTurmaDoAlunoPorUsuario($aluno->id).")</option>";
+        }
+
+        $usersQuery = $this->connection->query("
+            select distinct usuario.*, responsavel_por_aluno.id as rpa from usuario, aluno, responsavel_por_aluno
+            where aluno.usuario = usuario.id
+            and aluno.id = responsavel_por_aluno.aluno
+            and responsavel_por_aluno.responsavel = $responsavel->responsavel
+        ");
+        $usersQuery = $usersQuery->fetchAll(PDO::FETCH_OBJ);
+
+        $filhos = '';
+        foreach ($usersQuery as $user) {
+            $filhos .= "<tr id='row-$user->rpa'><td>$user->nome.'</td><td>".$this->util->pegarTurmaDoAlunoPorUsuario($user->id)."</td>
+            <td><button class='btn btn-danger btn-sm' id='deletar' value='$user->rpa'><span class='glyphicon glyphicon-remove'></span> Deletar</button></td></tr>";
+        }
+        
+        $args = [
+            'ID' => $responsavel->id,
+            'NOME' => $responsavel->nome,
+            'SALT' => $responsavel->salt,
+            'EMAIL' => $responsavel->email,
+            'ALUNOS' => $alunos,
+            'FILHOS' => $filhos
+        ];
+        
+        $template = $this->template->getTemplate('admin/responsavel.html');
+        $templateFinal = $this->template->parseTemplate($template, $args);
+        echo $templateFinal;
+    }
+    
+    public function adicionarResponsavel()
+    {   
+        $data = json_decode(json_encode($_POST), true);
+        
+        $email = $data['email'];
+        $nome = $data['nome'];
+        $password = $data['password'];
+        $salt = time() + rand(100, 1000);
+        $password = md5($password . $salt);
+        
+        if ($this->util->loginTakenBackEnd($email, "responsavel")) {
+            header('Location: /webschool/admin/responsaveis');
+            exit;
+        }
+        
+        $endereco = $this->connection->prepare("INSERT INTO endereco (estado) VALUES (:estado)");
+
+        $endereco->execute([
+            'estado' => 1,
+        ]);
+
+        $idEndereco = (int) $this->connection->lastInsertId();
+
+        $user = $this->connection->prepare("INSERT INTO usuario (nome, email, pass, salt, endereco)
+                VALUES (:name, :email, :password, :salt, :endereco)");
+
+        $user->execute([
+            'name' => $nome,
+            'email' => $email,
+            'password' => $password,
+            'salt' => $salt,
+            'endereco' => $idEndereco,
+        ]);
+
+        $userId = (int) $this->connection->lastInsertId();
+
+        $responsavel = $this->connection->prepare("INSERT INTO responsavel (usuario) VALUES (:idUusuario)");
+        $responsavel->execute([
+            'idUusuario' => $userId,
+        ]);
+
+        $avatar = $this->connection->prepare("INSERT INTO fotos_de_avatar (usuario) VALUES (:idUusuario)");
+        $avatar->execute([
+            'idUusuario' => $userId,
+        ]);
+
+        header('Location: /webschool/admin/responsaveis');
+    }
+    
+    public function atualizarResponsavel()
+    {
+        $data = json_decode(json_encode($_POST), true);
+
+        $userId = $data['id'];
+        $nome = $data['nome'];
+        $email = $data['email'];
+        $password = $data['password'];
+        $salt = $data['salt'];
+        $newPassword = md5($password);
+        
+        if ($this->util->loginTakenBackEnd($email, "responsavel", $userId)) {
+            header('Location: /webschool/admin/responsaveis');
+            exit;
+        }
+
+        $sql = "
+            UPDATE usuario
+            SET nome=:nome, email=:email
+            ";
+
+        $fields = [
+            'nome' => $nome,
+            'email' => $email,
+        ];
+
+        if (strlen($password) > 0) {
+            $password = $data['password'];
+            $password = md5($password . $salt);
+
+            $sql .= ' ,pass=:pass';
+            $fields['pass'] = $password;
+        }
+
+        $sql .= ' where id=:userId';
+        $fields['userId'] = $userId;
+
+        $user = $this->connection->prepare($sql);
+        $user->execute($fields);
+        
+        header('Location: /webschool/admin/responsaveis');
+    }
+    
+    public function removerResponsavel(int $idResponsavel)
+    {
+        $user = $this->connection->prepare("UPDATE usuario SET is_deleted = 1 WHERE id = :id");
+
+        $user->execute([
+            'id' => $idResponsavel,
+        ]);
+    }
+    
+    public function adicionarAlunoPorResponsavel()
+    {
+        $data = json_decode(json_encode($_POST), true);
+        
+        $responsavel = $data['id'];
+        $aluno = $data['aluno'];
+
+        $responsavelQuery = $this->connection->query("select id from responsavel where usuario = $responsavel");
+        $responsavelQuery = $responsavelQuery->fetchObject();
+
+        $alunoQuery = $this->connection->query("select id from aluno where usuario = $aluno");
+        $alunoQuery = $alunoQuery->fetchObject();
+
+        $user = $this->connection->prepare("INSERT INTO responsavel_por_aluno (responsavel, aluno)
+                VALUES (:responsavel, :aluno)");
+
+        $count = $user->execute([
+            'responsavel' => $responsavelQuery->id,
+            'aluno' => $alunoQuery->id,
+        ]);
+
+        header("Location: /webschool/admin/responsavel/$responsavel");
+    }
+    
+    public function removerAlunoPorResponsavel(int $id)
+    {
+        $user = $this->connection->prepare("DELETE from responsavel_por_aluno WHERE id = :id");
+
+        $user->execute([
+            'id' => $id,
+        ]);
+    }
+    
+    public function verTurmas()
+    {   
         $turmaQuery = $this->connection->query("select * from turma order by serie");
         $turmaQuery = $turmaQuery->fetchAll(PDO::FETCH_OBJ);
         
@@ -59,7 +258,7 @@ class AdminController {
           'ALUNOS' => $turmas  
         ];
         
-        $template 	= $this->template->getTemplate('admin/turmas.html');
+        $template = $this->template->getTemplate('admin/turmas.html');
         $templateFinal = $this->template->parseTemplate($template, $args);
         echo $templateFinal;
     }
@@ -77,7 +276,7 @@ class AdminController {
         
         $template 	= $this->template->getTemplate('admin/turma.html');
         $templateFinal = $this->template->parseTemplate($template, $args);
-        echo $templateFinal;        
+        echo $templateFinal;
     }
     
     public function adicionarTurma()
