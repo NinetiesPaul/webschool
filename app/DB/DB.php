05bedf49ca067515529bd0872467df54f9f5e966
Paul Richard <paulrpm@gmail.com>
2019-12-12 21:52:12 -0300

    getting dabase connection info from the enviroment

--- app/DB/DB.php
+++ app/DB/DB.php
@@ -10,10 +10,10 @@ class DB extends PDO
     
     public function __construct()
     {
-        $localhost_db = 'localhost';
-        $dbname_db = 'webschool';
-        $user_db = 'root';
-        $password_db = '';
+        $localhost_db = getenv('DB_HOST');
+        $dbname_db = getenv('DB_NAME');
+        $user_db = getenv('DB_USER');
+        $password_db = getenv('DB_PASSWORD');
         parent::__construct("mysql:host=$localhost_db; dbname=$dbname_db", $user_db, $password_db, []);
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
