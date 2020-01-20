d9e6a4b31a5fa9836b2456cefc8fe77bba8395c2
Paul Richard <paul.richard@bhut.com.br>
2020-01-20 14:43:53 -0300

    criou AdminStorage

---
+++ app/DB/Storage/AdminStorage.php
@@ -0,0 +1,133 @@
+<?php
+
+
+namespace App\DB;
+
+
+class AdminStorage
+{
+    public function verAlunos()
+    {
+
+    }
+
+    public function verAluno()
+    {
+
+    }
+
+    public function adicionarAluno()
+    {
+
+    }
+
+    public function alterarAluno()
+    {
+
+    }
+
+    public function removerAluno()
+    {
+
+    }
+
+    public function verProfessores()
+    {
+
+    }
+
+    public function verProfessor()
+    {
+
+    }
+
+    public function adicionarProfessor()
+    {
+
+    }
+
+    public function alterarProfessor()
+    {
+
+    }
+
+    public function removerProfessor()
+    {
+
+    }
+
+    public function verResponsaveis()
+    {
+
+    }
+
+    public function verResponsavel()
+    {
+
+    }
+
+    public function adicionarResponsavel()
+    {
+
+    }
+
+    public function alterarResponsavel()
+    {
+
+    }
+
+    public function removerResponsavel()
+    {
+
+    }
+
+    public function verTurmas()
+    {
+
+    }
+
+    public function verTurma()
+    {
+
+    }
+
+    public function adicionarTurma()
+    {
+
+    }
+
+    public function alterarTurma()
+    {
+
+    }
+
+    public function removerTurma()
+    {
+
+    }
+
+    public function verMaterias()
+    {
+
+    }
+
+    public function verMateria()
+    {
+
+    }
+
+    public function adicionarMateria()
+    {
+
+    }
+
+    public function alterarKMateria()
+    {
+
+    }
+
+    public function removerMateria()
+    {
+
+    }
+}
