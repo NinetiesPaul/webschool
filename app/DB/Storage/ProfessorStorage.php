2b623a16e5f40af0e32b2c14932eb42d354853c8
Paul Richard <paul.richard@bhut.com.br>
2020-01-20 14:44:11 -0300

    criou ProfessorStorage

---
+++ app/DB/Storage/ProfessorStorage.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\DB;
+
+
+class ProfessorStorage
+{
+    public function verTurmas()
+    {
+
+    }
+
+    public function verTurma()
+    {
+
+    }
+
+    public function verDiarioDeClasse()
+    {
+
+    }
+}
