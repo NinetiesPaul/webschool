1240e5f6f9987a965842e568c9f4ddb95af4d30c
Paul Richard <paul.richard@bhut.com.br>
2020-01-21 12:25:54 -0300

    adicionado métodos em AlunoStorage

--- app/DB/Storage/AlunoStorage.php
+++ app/DB/Storage/AlunoStorage.php
@@ -1,12 +1,36 @@
 <?php
 
 
-namespace App\DB;
+namespace App\DB\Storage;
 
+use App\DB\DB;
+use PDO;
 
-class AlunoStorage
+class AlunoStorage extends DB
 {
-    public function verTurmas()
+    public function verAlunos()
+    {
+        $alunoQuery = $this->connect()->query("select usuario.* from usuario, aluno where usuario.id=aluno.usuario");
+        return $alunoQuery->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function verAluno(int $aluno)
+    {
+        $alunoQuery = $this->connect()->query("select usuario.* from usuario, aluno where usuario.id=aluno.usuario and aluno.usuario = $aluno");
+        return $alunoQuery->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function adicionarAluno()
+    {
+
+    }
+
+    public function alterarAluno()
+    {
+
+    }
+
+    public function removerAluno()
     {
 
     }
