92d3a4349d780c17ef1207e36de5b44f8fa19bad
Paul Richard <paulrpm@gmail.com>
2020-01-22 20:13:55 -0300

    simple code refactoring

--- app/DB/Storage/AdminStorage.php
+++ app/DB/Storage/AdminStorage.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\DB;
-
 
 class AdminStorage extends DB
 {
--- app/DB/Storage/TurmaStorage.php
+++ app/DB/Storage/TurmaStorage.php
@@ -1,21 +1,10 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App\DB\Storage;
 
-use APP\DB\DB;
+use App\DB\DB;
 use PDO;
 
-/**
- * Description of TurmaStorage
- *
- * @author Paul Richard
- */
 class TurmaStorage extends DB {
 
     public function verTurmas()
