085595dc536e9d6fbc674fab1bd564ed074541ee
Paul Richard <paulrpm@gmail.com>
2020-01-22 20:15:09 -0300

    created EnderecoStorage

--- app/DB/Storage/EnderecoStorage.php
+++ app/DB/Storage/EnderecoStorage.php
@@ -1,12 +1,27 @@
 <?php
 
-
-namespace App\DB;
+namespace App\DB\Storage;
 
 use App\DB\DB;
 use PDO;
 
 class EnderecoStorage extends DB
 {
+    public $localConnection;
+    
+    public function __construct() {
+        parent::__construct();
+        $this->localConnection = $this->connect();   
+    }
+    
+    public function inserirEndereco()
+    {   
+        $endereco = $this->localConnection->prepare("INSERT INTO endereco (estado) VALUES (:estado)");
 
+        $endereco->execute([
+            'estado' => 1,
+        ]);
+
+        return (int) $this->localConnection->lastInsertId();
+    }
 }
