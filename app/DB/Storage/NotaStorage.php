aeef45feb0451719858b1b88588a069bbb3b1260
Paul Richard <paul.richard@bhut.com.br>
2020-01-24 09:28:42 -0300

    added more methods on AlunoStorage

--- app/DB/Storage/NotaStorage.php
+++ app/DB/Storage/NotaStorage.php
@@ -12,4 +12,16 @@ class NotaStorage extends DB
         $save = $this->connect()->prepare("INSERT INTO nota_por_aluno (aluno, disciplina, turma, nota1, nota2, nota3, nota4, rec1, rec2, rec3, rec4) VALUES (:idAluno, :idDisciplina, :idTurma, 0, 0, 0, 0, 0, 0, 0, 0)");
         $save->execute($nota);
     }
+
+    public function verTurmasComNotaDoAluno($aluno)
+    {
+        $exec = $this->connect()->query("select turma from nota_por_aluno where aluno=$aluno group by turma");
+        return $exec->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function verNotasPorTruma($aluno, $turma)
+    {
+        $exec = $this->connect()->query("select * from nota_por_aluno where aluno=$aluno and turma=$turma order by disciplina");
+        return $exec->fetchAll(PDO::FETCH_OBJ);
+    }
 }
