89c9ff89af9e3414399548cc0729e4ebfe693bde
Paul Richard <paulrpm@gmail.com>
2020-05-22 00:40:57 -0300

    removed moved method

--- app/Util.php
+++ app/Util.php
@@ -24,58 +24,6 @@ class Util
             header('Location: /webschool/');
         }
     }
-    
-    public function loginTakenAjax()
-    {
-        $data = json_decode(json_encode($_POST), true);
-        
-        $login = $data["login"];
-        $tipo = $data["tipo"];
-        $id = (isset($data["id"])) ? $data['id'] : null;
-        
-        $query = "
-            SELECT usuario.id FROM usuario,$tipo
-            WHERE usuario.id = $tipo.usuario
-            and usuario.email = '$login'
-        ";
-
-        if ($id) {
-            $query .= " and usuario.id != $id";
-        }
-
-        $cidadeQuery = $this->connection->query($query);
-        $cidadeQuery = $cidadeQuery->fetchObject();
-
-        $res = false;
-        if ($cidadeQuery) {
-            $res = true;
-        }
-
-        echo $res;
-    }
-
-    public function loginTakenBackEnd(string $login, string $tipo, $id = false)
-    {
-        $query = "
-            SELECT usuario.id FROM usuario,$tipo
-            WHERE usuario.id = $tipo.usuario
-            and usuario.email = '$login'
-        ";
-
-        if ($id) {
-            $query .= " and usuario.id != $id";
-        }
-
-        $userQuery = $this->connection->query($query);
-        $userQuery = $userQuery->fetchObject();
-
-        $res = false;
-        if ($userQuery) {
-            $res = true;
-        }
-
-        return $res;
-    }
 
     public function loadTemplate($path, $args = null)
     {
