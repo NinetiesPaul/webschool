80aaa923193e66b11d130b372a1c2b38fb3d40ac
Paul Richard <paulrpm@gmail.com>
2024-02-07 23:13:47 -0300

    refactor: classe templates em cargo de gerar links de menus, links de css e links de js

--- app/Util.php
+++ app/Util.php
@@ -19,44 +19,4 @@ class Util
             header("Location: /");
         }
     }
-
-    public static function generateLinks($nivel = '', $perfil = false)
-    {
-        session_start();
-        $links = '';
-
-        if (isset($_SESSION['tipo'])) {
-            $tipo = $_SESSION['tipo'];
-
-            $path = ($perfil) ? $tipo . "/" . $nivel : $nivel;
-
-
-            if ($tipo == Enum::TIPO_ADMIN) {
-                $links = "
-                    <a class='btn btn-light btn btn-block' href='".$path."turmas'>Turmas</a>
-                    <a class='btn btn-light btn btn-block' href='".$path."disciplinas'>Disciplinas</a>
-                    <a class='btn btn-light btn btn-block' href='".$path."professores'>Professores</a>
-                    <a class='btn btn-light btn btn-block' href='".$path."alunos'>Alunos</a>
-                    <a class='btn btn-light btn btn-block' href='".$path."responsaveis'>Responsáveis</a>
-                ";
-            }
-            if ($tipo == Enum::TIPO_RESPONSAVEL) {
-                $links = "
-                    <a class='btn btn-light btn btn-block' href='".$path."alunos'>Alunos</a>
-                ";
-            }
-            if ($tipo == Enum::TIPO_PROFESSOR) {
-                $links = "
-                    <a class='btn btn-light btn btn-block' href='".$path."turmas'>Turmas</a>
-                ";
-            }
-            if ($tipo == Enum::TIPO_ALUNO) {
-                $links = "
-                    <a class='btn btn-light btn btn-block' href='".$path."turmas'>Turmas</a>
-                ";
-            }
-        }
-
-        return $links;
-    }
 }
