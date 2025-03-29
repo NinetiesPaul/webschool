a7e6fa226cd4af120ea1ea7a541c843a66ce8dc4
Paul Richard <paulrpm@gmail.com>
2025-03-29 18:54:49 -0300

    feat: criado teste AlunoStorageTest

---
+++ tests/AlunoStorageTest.php
@@ -0,0 +1,263 @@
+<?php declare(strict_types=1);
+
+use App\DB\Storage\AlunoStorage;
+use PHPUnit\Framework\TestCase;
+
+class AlunoStorageTest extends TestCase
+{
+    public function testVerAlunosRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetchAll')
+            ->willReturn([]);
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->verAlunos();
+
+        $this->assertIsArray($alunos);
+    }
+
+    public function testVerAlunoRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetch')
+            ->willReturn(
+                (object)['id' => 1, 'nome' => 'Turma A', 'ano' => 2021],
+                );
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->verAluno(1);
+
+        $this->assertInstanceOf(stdClass::class, $alunos);
+    }
+
+    public function testDesativarAlunoRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $pdoMock->expects($this->once())
+            ->method('prepare')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetch')
+            ->willReturn(
+                (object)['is_deleted' => 1],
+            );
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->desativarAluno(1);
+
+        $this->assertEquals(true, true);
+    }
+
+    public function testDesativarAlunoRetornoExcecao(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $pdoMock->expects($this->once())
+            ->method('prepare')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetch')
+            ->willReturn(
+                (object)[ 'is_deleted' => 1 ],
+            );
+
+        $stmtMock->expects($this->exactly(2))
+            ->method('rowCount')
+            ->willReturnOnConsecutiveCalls(1, 0);  
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $this->expectException(Exception::class);
+        $alunoMock->desativarAluno(1);
+    }
+
+    public function testDesativarAlunoRetornoUsuarioDesconhecido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('rowCount')
+            ->willReturn(0);
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $this->expectException(Exception::class);
+        $alunoMock->desativarAluno(1);
+    }
+
+    public function testVerAlunoDoResponsavelRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetchAll')
+            ->willReturn([]);
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->verAlunosDoResponsavel(1);
+
+        $this->assertIsArray($alunos);
+    }
+
+    public function testPegarNomeDoAlunoPorAlunoIdRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetch')
+            ->willReturn(
+                (object) [ 'nome' => 'nome_do_aluno' ]
+            );
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->pegarNomeDoAlunoPorAlunoId(1);
+
+        $this->assertIsString($alunos);
+
+        $this->assertEquals('nome_do_aluno', $alunos);
+    }
+
+    public function testPegarIdDaTurmaDoAlunoPorAlunoIdRetornoValido(): void
+    {
+        $pdoMock = $this->createMock(PDO::class);
+
+        $stmtMock = $this->createMock(PDOStatement::class);
+
+        $pdoMock->expects($this->once())
+            ->method('query')
+            ->willReturn($stmtMock);
+
+        $stmtMock->expects($this->once())
+            ->method('fetch')
+            ->willReturn(
+                (object) [ 'turma' => 1 ]
+            );
+
+        $alunoMock = $this->getMockBuilder(AlunoStorage::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__construct'])
+            ->getMock();
+
+        $reflection = new ReflectionClass(AlunoStorage::class);
+        $dbProperty = $reflection->getParentClass()->getProperty('db');
+        $dbProperty->setAccessible(true);
+        $dbProperty->setValue($alunoMock, $pdoMock);
+
+        $alunos = $alunoMock->pegarIdDaTurmaDoAlunoPorAlunoId(1);
+
+        $this->assertIsInt($alunos);
+
+        $this->assertEquals(1, $alunos);
+    }
+}
