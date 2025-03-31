f5dc797033bd624c3a76202cf8541d8b3fe26c47
Paul Richard <paulrpm@gmail.com>
2025-03-31 15:06:11 -0300

    feat: refatorado codigo chamando metodos externos

--- app/DB/Storage/ResponsavelStorage.php
+++ app/DB/Storage/ResponsavelStorage.php
@@ -32,15 +32,14 @@ class ResponsavelStorage extends DB
     
     public function adicionarResponsavel($email, $nome, $password, $salt)
     {
-        $usuarioStorage = new UsuarioStorage();
+        $usuarioStorage = new UsuarioStorage($this->db);
         if ($usuarioStorage->loginTaken($email, Enum::TIPO_RESPONSAVEL)) {
             return false;
         }
 
-        $enderecoStorage = new EnderecoStorage();
+        $enderecoStorage = new EnderecoStorage($this->db);
         $idEndereco = $enderecoStorage->inserirEndereco();
 
-        $usuarioStorage = new UsuarioStorage();
         $userId = $usuarioStorage->inserirUsuario([
             'name' => $nome,
             'email' => $email,
@@ -54,27 +53,18 @@ class ResponsavelStorage extends DB
             'idUusuario' => $userId,
         ]);
 
-        $avatarStorage = new AvatarStorage();
+        $avatarStorage = new AvatarStorage($this->db);
         $avatarStorage->inserirUsuarioNaAvatar($userId);
     }
 
     // todo: refatorar metodo e quebrar cada chamada de deleção para o storage pertinente
     public function removerResponsavel($responsavel, $usuario, $endereco, $footprint)
     {
-        $user = $this->db->prepare("UPDATE usuario SET endereco = NULL WHERE id = :id;");
-        $user->execute([
-            'id' => $usuario,
-        ]);
+        $enderecoStorage = new EnderecoStorage($this->db);
+        $enderecoStorage->deletarEndereco($usuario, $endereco);
 
-        $user = $this->db->prepare("DELETE FROM endereco WHERE id = :endereco;");
-        $user->execute([
-            'endereco' => $endereco,
-        ]);
-
-        $user = $this->db->prepare("DELETE FROM fotos_de_avatar WHERE usuario = :id;");
-        $user->execute([
-            'id' => $usuario,
-        ]);
+        $avatarStorage = new AvatarStorage($this->db);
+        $avatarStorage->deletarAvatarDoUsuario($usuario);
 
         $user = $this->db->prepare("DELETE FROM responsavel_por_aluno WHERE responsavel = :aluno;");
         $user->execute([
@@ -86,10 +76,8 @@ class ResponsavelStorage extends DB
             'responsavel' => $responsavel,
         ]);
 
-        $user = $this->db->prepare("DELETE FROM usuario WHERE id = :id;");
-        $user->execute([
-            'id' => $usuario,
-        ]);
+        $usuarioStorage = new UsuarioStorage($this->db);
+        $usuarioStorage->deletarUsuario($usuario);
         
         $footprintBackup = $this->db->prepare("INSERT INTO usuarios_deletados (tipo, nome, footprint, deletado_em) VALUES ('responsavel', :nome, :footprint, NOW())");
         $footprintBackup->execute([
