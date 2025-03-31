70b85fb822144e04d9d0b3622d2b770afe813253
Paul Richard <paulrpm@gmail.com>
2025-03-31 15:06:27 -0300

    feat: reutilizando objetos PDO

--- app/DB/DB.php
+++ app/DB/DB.php
@@ -8,16 +8,20 @@ class DB
 {
     protected $db;
 
-    public function __construct()
+    public function __construct(?PDO $db = null)
     {
-        $localhost_db = getenv('DB_HOST');
-        $dbname_db = getenv('DB_NAME');
-        $user_db = getenv('DB_USER');
-        $password_db = getenv('DB_PASSWORD');
+        if ($db) {
+            $this->db = $db;
+        } else {
+            $localhost_db = getenv('DB_HOST');
+            $dbname_db = getenv('DB_NAME');
+            $user_db = getenv('DB_USER');
+            $password_db = getenv('DB_PASSWORD');
 
-        $pdo = new PDO("mysql:host=$localhost_db; dbname=$dbname_db", $user_db, $password_db, []);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo = new PDO("mysql:host=$localhost_db; dbname=$dbname_db", $user_db, $password_db, []);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $this->db = $pdo;
+            $this->db = $pdo;
+        }
     }
 }
